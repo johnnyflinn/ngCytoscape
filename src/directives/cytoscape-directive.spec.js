17b88eff1117dc821986c857932f61167fffa3eb
Johnny <johnny.l.flinn@gmail.com>
2016-02-15 12:28:07 -0500

    renamed test file

--- src/directives/cytoscape-directive.spec.js
+++ src/directives/cytoscape-directive.spec.js
@@ -7,7 +7,7 @@ describe('Directive: cytoscape', function(){
     var scope;
     var CytoscapeGraph;
     var cytoData;
-beforeEach(module('ngCytoscape'));
+    beforeEach(module('ngCytoscape'));
     beforeEach(inject(function(_$compile_, _$rootScope_, _cytoData_, _CytoscapeGraph_){
         $compile = _$compile_;
         $rootScope = _$rootScope_;
@@ -22,14 +22,21 @@ beforeEach(module('ngCytoscape'));
         $rootScope.$apply();
     }));
     it('should have loaded cytoscape inside the directive', function() {
-        //var isolated = element.isolateScope();
-
-
+        var element = angular.element('<cytoscape></cytoscape>');
+        element = $compile(element)(scope);
+        scope.$digest();
+        expect(element.find('canvas').length).toEqual(3);
     });
-    it('should add elements', function() {
-      // console.log(scope)
-    });
-    it('should run layout', function() {
-      // console.log(scope)
+    it('should set defaults', function() {
+        angular.extend(scope, {defaults:{zoomingEnabled:false}});
+        var element = angular.element('<cytoscape graph-options="defaults"></cytoscape>');
+        element = $compile(element)(scope);
+        var cyGraph;
+        cytoData.getGraph().then(function(graph){
+            cyGraph = graph;
+        });
+        scope.$digest();
+        console.log(cyGraph.zoomingEnabled());
+        expect(cyGraph.zoomingEnabled()).toEqual(false);
     });
 });
