34a354d7655dcec732582244b5c8f7d7c3237bcf
C. Kapopoulos <ckapopoulos@aptima.com>
2017-07-31 12:56:43 -0400

    Fix typo.

--- src/directives/cytoscape-directive.js
+++ src/directives/cytoscape-directive.js
@@ -68,7 +68,7 @@
         function linkFn(scope,element,attrs,ctrlFn){
             var isDefined = cytoHelpers.isDefined;
             var isEmpty = cytoHelpers.isEmpty;
-            cytoGraphDefaults.setDefaults(scope.graphOptions, scope.graphLayout, attrs.id, scope.graphStyle, scope.graphExtentions);
+            cytoGraphDefaults.setDefaults(scope.graphOptions, scope.graphLayout, attrs.id, scope.graphStyle, scope.graphExtensions);
             scope.graphId =  attrs.id;
             var cy = new CytoscapeGraph(element[0], cytoGraphDefaults.getGraphCreationDefaults(attrs.id));
             cytoEvents.setEvents(cy);
