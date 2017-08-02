8345b1c30f457e9135c2374803e7e729cd028b18
C. Kapopoulos <ckapopoulos@aptima.com>
2017-08-02 11:33:59 -0400

    Changed context of extension call.

--- dist/ngCytoscape.js
+++ dist/ngCytoscape.js
@@ -84,7 +84,7 @@
             if (isDefined(scope.graphExtensions)) {
                 angular.forEach(scope.graphExtensions, function(ele, index){
                     // cy[ele.extension](ele.options);
-                    cytoHelpers.executeFunctionByName(ele.extension, cy, ele.options);
+                    cytoHelpers.executeFunctionByName(ele.extension, 'cytoscape', ele.options);
                 });
             }
 
@@ -245,7 +245,7 @@
                             if (isDefined(defaults.extensions)) {
                                 angular.forEach(defaults.extensions, function(ele, index){
                                     //graph[ele.extension](ele.options);
-                                    cytoHelpers.executeFunctionByName(ele.extension, graph, ele.options);
+                                    cytoHelpers.executeFunctionByName(ele.extension, 'cytoscape', ele.options);
                                 });
                             }
                         }
